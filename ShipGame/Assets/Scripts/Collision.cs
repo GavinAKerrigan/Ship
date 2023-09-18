0a50d034c0ce9d5edc2ae4075eb500b8866027d0
Gavin Kerrigan (RIT Student) <gak6718@rit.edu>
2023-09-18 11:46:28 -0400

    Added polygon collision to android and rigidbody 2d

--- ShipGame/Assets/Scripts/Collision.cs
+++ ShipGame/Assets/Scripts/Collision.cs
@@ -7,21 +7,21 @@ public class Collision : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
-    {  }
+    { }
 
     // Update is called once per frame
     void Update()
-    {}
+    { }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Victory")
         {
-
+            //SceneManager.LoadScene(sceneName: "Victory");
         }
         else
         {
-
+            //SceneManager.LoadScene(sceneName: "Level");
         }
     }
 }
