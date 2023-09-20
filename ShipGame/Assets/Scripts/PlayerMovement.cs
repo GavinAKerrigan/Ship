b84bdbd347852a9b5cf6a9e47950f9652deeb959
GinoLeone117 <gdl4358@rit.edu>
2023-09-20 11:50:36 -0400

    Player Movement

---
+++ ShipGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D ship;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("space"))
+        {
+            Thrust();
+        }
+
+        if (Input.GetKey("a")) {
+            Rotate();
+        }
+    }
+
+    public void Thrust()
+    {
+        
+            //Rocket thrusts
+            Debug.Log("Thrusting");
+    
+    }
+
+    public void RotateLeft()
+    {/*
+        if (Input.GetKey("a"))
+        {
+            //ship rotates left
+        }   else if (Input.GetKey("left"))
+        {
+            //ship rotates left
+        }
+
+        if (Input.GetKey("d"))
+        {
+            //ship rotates right
+
+        }   else if (Input.GetKey("right"))
+        {
+            //ship rotates right
+        }
+        */
+        Debug.Log("rotating left");
+    }
+
+    public void RotateRight()
+    {
+        //ship rotates right
+        Debug.Log("rotating right");
+    }
+}
