5640fe301924f43b3033a3a5327c2bbbb3131a29
Sam Ruggiero <85330201+SJRuggs@users.noreply.github.com>
2023-10-06 11:56:29 -0400

    Revert "Gino"

--- ShipGame/Assets/Scripts/PlayerMovement.cs
+++ ShipGame/Assets/Scripts/PlayerMovement.cs
@@ -8,26 +8,24 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D ship;
 
     //Assign values here based on how we want the ship to operate. Add other variables that you believe we need for physics
-    float thrustSpeed;
-    float rotationSpeed;
+    private float thrustSpeed;
+    private float rotationSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
         ship = GetComponent<Rigidbody2D>();
-        thrustSpeed = 1.7f;
-        rotationSpeed = 1.2f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.Space))
         {
             //Rocket thrusts
             Thrust();
         }
-        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.LeftShift))
+        else if (Input.GetKey(KeyCode.S))
         {
             Stablizer();
         }
@@ -40,26 +38,24 @@ public class PlayerMovement : MonoBehaviour
         {
             RotateRight();
         }
-
-        autoRotationStablizer();
     }
 
     public void Thrust()
     {
-        ship.AddRelativeForce((Vector2.up / 5) * thrustSpeed);
+        ship.AddRelativeForce(Vector2.up / 5);
         //Debug.Log("Thrusting");
     }
 
     public void RotateLeft()
     {
         //transform.Rotate(0, 0, .2f);
-        ship.angularVelocity += rotationSpeed;
+        ship.angularVelocity += .4f;
         // Debug.Log("rotating left");
     }
 
     public void RotateRight()
     {
-        ship.angularVelocity -= rotationSpeed;
+        ship.angularVelocity -= .4f;
         // Debug.Log("rotating right");
     }
 
@@ -67,10 +63,5 @@ public class PlayerMovement : MonoBehaviour
     {
         ship.velocity = ship.velocity / 1.001f;
         // Debug.Log("Destablizing");
-    }
-
-    public void autoRotationStablizer()
-    {
-        ship.angularVelocity = ship.angularVelocity / 1.003f;
     }
 }
