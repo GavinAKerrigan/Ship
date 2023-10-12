4f9c4a5bc8f2f8728c86b83dc919b4d5f79b3a9c
SJRuggs <sjruggs03@gmail.com>
2023-10-12 15:51:17 -0400

    level 2 functional
    added reverse buttons

--- ShipGame/Assets/Scripts/Player.cs
+++ ShipGame/Assets/Scripts/Player.cs
@@ -12,10 +12,11 @@ public class Player : MonoBehaviour
     SpriteRenderer spriteRenderer;
 
     // controls references
-    private List<KeyCode> rotateLeftKeys, rotateRightKeys, stabilizeKeys, thrustKeys;
+    private List<KeyCode> rotateLeftKeys, rotateRightKeys, reverseKeys, stabilizeKeys, thrustKeys;
 
     // serialized ship stats
     [SerializeField] float  thrust,             // thrusting power
+                            reverse,            // reverse thrusting power
                             rotation,           // turning power
                             stablizer,          // stabilizing power
                             maxVelocity,        // maximum velocity
@@ -25,6 +26,7 @@ public class Player : MonoBehaviour
                             afterburn;          // afterburner strength (latent thrust)
     // internal ship stats
     private float   iThrust,                // adjusted thrusting power
+                    iReverse,               // adjusted reverse thrusting power
                     iRotation,              // adjusted turning power
                     iStablizer,             // adjusted stabilizing power
                     iMaxVelocity,           // adjusted maximum velocity
@@ -43,11 +45,13 @@ public class Player : MonoBehaviour
         // set up control scheme
         thrustKeys      = new List<KeyCode> { KeyCode.W, KeyCode.I, KeyCode.UpArrow,    KeyCode.Space };
         rotateLeftKeys  = new List<KeyCode> { KeyCode.A, KeyCode.J, KeyCode.LeftArrow };
-        stabilizeKeys   = new List<KeyCode> { KeyCode.S, KeyCode.K, KeyCode.DownArrow,  KeyCode.LeftShift };
+        reverseKeys     = new List<KeyCode> { KeyCode.S, KeyCode.K, KeyCode.DownArrow };
         rotateRightKeys = new List<KeyCode> { KeyCode.D, KeyCode.L, KeyCode.RightArrow };
+        stabilizeKeys   = new List<KeyCode> { KeyCode.LeftShift,    KeyCode.RightShift };
 
         // modify ship stats to reflect real values
-        iThrust              = thrust               * 0.1f;
+        iThrust              = thrust               * 0.08f;
+        iReverse             = iThrust              * 0.2f;
         iRotation            = rotation             * 0.1f;
         iStablizer           = 1 - stablizer        * 0.0005f;
         iMaxVelocity         = maxVelocity          * 2f;
@@ -75,7 +79,9 @@ public class Player : MonoBehaviour
     /// interperates the controls and calls the appropriate functions
     private void Move() {
         bool thrusting = false;
+        bool reversing = false;
         foreach (KeyCode key in thrustKeys)         if (Input.GetKey(key)) { thrusting = true;                  break; }
+        foreach (KeyCode key in reverseKeys)        if (Input.GetKey(key)) { reversing = true;                  break; }
         foreach (KeyCode key in rotateLeftKeys)     if (Input.GetKey(key)) { ship.angularVelocity += iRotation; break; }
         foreach (KeyCode key in rotateRightKeys)    if (Input.GetKey(key)) { ship.angularVelocity -= iRotation; break; }
         foreach (KeyCode key in stabilizeKeys)      if (Input.GetKey(key)) { 
@@ -85,12 +91,17 @@ public class Player : MonoBehaviour
         }
         
         // apply thrust or afterburner
-        if (thrusting)  { ship.AddRelativeForce(Vector2.up * iThrust);       iLastThrust  = iThrust; }      // default thrust
-        else            { ship.AddRelativeForce(Vector2.up * iLastThrust);   iLastThrust *= iAfterburn; }   // afterburner
+        if (thrusting)  { ship.AddRelativeForce(Vector2.up * iThrust);       iLastThrust  = iThrust; } // default thrust
+        else {
+            ship.AddRelativeForce(Vector2.up * iLastThrust);  
+            iLastThrust *= iAfterburn;  // afterburner
+            if (reversing) ship.AddRelativeForce(Vector2.down * iReverse);  // reverse thrust                
+        }   
     }
 
-    // special collision handler
+    // collision handler
     public void OnTriggerEnter2D(Collider2D collision) {
+
         if (collision.gameObject.tag == "Victory")
         {
             spriteRenderer.color = Color.green;
