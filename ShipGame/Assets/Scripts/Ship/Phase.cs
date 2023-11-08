c9f7840b3532849eb3ebfa0ca8e73e63cc0608b0
SJRuggs <85330201+SJRuggs@users.noreply.github.com>
2023-11-08 11:42:15 -0500

    fixed hud

---
+++ ShipGame/Assets/Scripts/Ship/Phase.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Phase : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+
+    void Awake()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
